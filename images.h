df876727e4449cb3d7beaa9d734d07be0a823387
MuAbCh <mabdullahc19@gmail.com>
2024-04-07 10:45:21 -0400

    fixed glitch of game not working properly after every level completed

--- images.h
+++ images.h
@@ -6,6 +6,13 @@
 // level images
 extern unsigned short Level1[76800]; // level 1 image
 extern unsigned short Level2[76800]; // Level 2 image
+extern unsigned short Level_3[76800]; // Level 3 image
+extern unsigned short Level_5_end[76800]; // Level 5 end image
+extern unsigned short flag[1760]; // flag image
+extern unsigned short Super_Ifti_Bros_start_screen[76800]; // start screen image
+extern unsigned short flag_bd[1760]; // flag image
+extern unsigned short flag_bd_flipped[1760]; // flag image
+extern unsigned short game_over_success[76800]; // game over success image
 
 // Iftikher images
 extern unsigned short Iftikher_still_right[1600]; // ifitkher still image right
@@ -26,5 +33,10 @@ extern unsigned short Iftier_run_right[1600]; // Iftier running image
 // power up images
 extern unsigned short transfrom_power_up[1600]; // power up image to transfrom into Iftier
 extern unsigned short transfrom_power_up_message[1600]; // image to show the message to transfrom into Iftier
+extern unsigned short super_jump_message[2800]; // super jump message image
+
+// sound files
+extern int game_cleared[46184]; // game cleared sound
+extern int start_sound[22151]; // start sound
 
 #endif
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
+// code put together in one file in roder to test on cpulator
 #include <stdbool.h>  // Include the standard library for boolean values
 #include <stdint.h>   // Include this header for using uint16_t type
 #include <stdlib.h>   // Include the standard library for rand()
 
-#include "images.h"  // Include the header file for the image arrays
+#include "images.h"
 
 volatile int pixel_buffer_start;  // global variable
 short int Buffer1[240][512];      // 240 rows, 512 (320 + padding) columns
@@ -10,19 +11,43 @@ short int Buffer2[240][512];
 
 int player_speed = 5;
 int platform = 175;
+int flag_y = 90;
+int flagWaveCount = 0;
+
 bool is_running_right = false;
 bool is_running_left = false;
 bool face_left = false;
-bool face_right = true;  // start facing right by default
+bool face_right = true;
 bool is_jumping = false;
 bool top_of_jump = false;
+
 bool on_lvl1_pipe = false;
-bool s_clicked = false;
+bool on_lvl2_pipe = false;
+bool on_lvl3_pipe = false;
+bool on_lvl5_pipe = false;
+
+//Control levels/stages of game
 bool level_1 = true;
 bool level_2 = false;
+bool level_3 = false;
+bool level_5_end = false;
+bool flag_takedown = false;
+bool game_complete = false;
+
 bool falling_animation_of_level2 = true;
 bool power_up_picked_up = false;
 bool Iftiluigi = false;
+bool isStartScreen = true;
+bool superJump = false;
+bool flagWave = false;
+bool playStartSound = true;
+
+bool isGameOverWait = false;
+bool isGameOverScreen = false;
+
+bool spaceBarPressed = false;
+bool enterBarPressed = false;
+bool s_clicked = false;
 
 // Define the struct for colors
 struct color {
@@ -58,6 +83,18 @@ struct color colors = {
     0x8410   // Gray
 };
 
+struct audio_t {
+	volatile unsigned int control;
+	volatile unsigned char rarc;
+	volatile unsigned char ralc;
+	volatile unsigned char warc;
+	volatile unsigned char walc;
+    volatile unsigned int ldata;
+	volatile unsigned int rdata;
+};
+
+struct audio_t *const audiop = ((struct audio_t *)0xff203040);
+
 void swap(int *a, int *b);
 void draw_box(int x, int y, int short line_color);
 void wait_for_vsync();
@@ -72,8 +109,13 @@ void draw_iftimario_running_right(x_offset);
 void draw_iftimario_running_left(x_offset);
 void draw_iftimario_jumping_right(x_offset, y_offset);
 void draw_iftimario_jumping_left(x_offset, y_offset);
-void draw_transform_power_up();
+void draw_transform_power_up(int x_offset, int y_offset);
 void draw_transform_power_up_message();
+void draw_super_jump_power_up_message();
+void audio_playback_mono(int *samples, int n);
+void draw_flag(x_offset, y_offset);
+void draw_flag_bd(x_offset, y_offset);
+void draw_game_over_success();
 
 int main(void) {
   // The x value holder for moving the character
@@ -118,8 +160,25 @@ int main(void) {
   while (1) {
     // level 1
     if (level_1) {
+      //reset variables in game
+      flag_y = 90;
+      power_up_picked_up = false;
+      if (isStartScreen) {
+        x_offset = 0;
+      }
+
       // draw the background again in order to erase previous stuff
       draw_level_1();
+
+      // Display Start Screen graphics
+      if (isStartScreen) {
+        draw_start_screen();
+      }
+
+      if (playStartSound) {
+        audio_playback_mono(start_sound, 22151);
+        playStartSound = false;
+      }
 
       // ********* logic for PS/2 Keyboard *********
       // Keyboard input processing. Keyboard input is read one byte at a time.
@@ -134,18 +193,21 @@ int main(void) {
         byte2 = byte1;
         byte1 = (ps2_data & 0xFF);
 
-        if (byte1 == 0x23) {
+        if (byte1 == 0x29) {
+         // Space = Start game
+         isStartScreen = false;
+        } else if (byte1 == 0x23 && !isStartScreen) {
           // D = move to the right
           is_running_right = true;
           if (x_offset < 295) x_offset += player_speed;
-        } else if (byte1 == 0x1C) {
+        } else if (byte1 == 0x1C && !isStartScreen) {
           // A = move to the left
           is_running_left = true;
           if (x_offset > -15) x_offset -= player_speed;
-        } else if (byte1 == 0x1D) {
+        } else if (byte1 == 0x1D && !isStartScreen) {
           // W = jump
           is_jumping = true;
-        } else if (byte1 == 0x1B) {
+        } else if (byte1 == 0x1B && !isStartScreen) {
           // S = go down pipe
           s_clicked = true;
         }
@@ -160,6 +222,7 @@ int main(void) {
         is_running_right = false;
         is_running_left = false;
         s_clicked = false;
+        spaceBarPressed = false;
       }
       // ********* end of logic for PS/2 Keyboard *********
 
@@ -296,7 +359,7 @@ int main(void) {
 
       // logic for picking up Iftiluigi power up
       if (!power_up_picked_up) {
-        draw_transform_power_up();
+        draw_transform_power_up(140, 150);
       }
       if (x_offset > 120 && x_offset < 160 && y_offset < 30) {
         if (!power_up_picked_up) {
@@ -342,18 +405,382 @@ int main(void) {
           }
         }
       }
+
+      if (x_offset >= 231 && platform == 150) {
+        x_offset = 231;
+
+        on_lvl2_pipe = true;
+        
+        if (on_lvl2_pipe) {
+          level_2 = false;
+          level_3 = true;
+          y_offset = 0;
+          x_offset = 20;
+          platform = 167;
+          power_up_picked_up = false;
+        }
+      }
+
+    } else if (level_3) {
+      draw_level_3();
+
+      // ********* logic for PS/2 Keyboard *********
+      // Keyboard input processing. Keyboard input is read one byte at a time.
+      // Every time a read is performed of the PS/2 control register, it
+      // discards the last byte.
+      ps2_data = *(ps2_ctrl_ptr);
+      validRead = ((ps2_data & 0x8000) != 0);
+
+      if (validRead) {
+        // Update the last 3 bytes read to reflect the current read.
+        byte3 = byte2;
+        byte2 = byte1;
+        byte1 = (ps2_data & 0xFF);
+
+        if (byte1 == 0x23) {
+          // D = move to the right
+          is_running_right = true;
+          if (x_offset < 295) x_offset += player_speed;
+        } else if (byte1 == 0x1C) {
+          // A = move to the left
+          is_running_left = true;
+          if (x_offset > -15) x_offset -= player_speed;
+        } else if (byte1 == 0x1D) {
+          // W = jump
+          is_jumping = true;
+        } else if (byte1 == 0x1B) {
+          // S = go down pipe
+          s_clicked = true;
+        } else if (byte1 == 0x2C) {
+          // T = pick up power up
+          t_clicked = true;
+        }
+      } else {
+        if (is_running_right) {
+          face_right = true;
+          face_left = false;
+        } else if (is_running_left) {
+          face_left = true;
+          face_right = false;
+        }
+        is_running_right = false;
+        is_running_left = false;
+        s_clicked = false;
+        t_clicked = false;
+      }
+      // ********* end of logic for PS/2 Keyboard *********
+
+      if (is_jumping) {
+        if (up) {
+          if (superJump) {
+            y_offset -= 10;
+          } else {
+            y_offset -= 5;
+          }
+        } else {
+          y_offset += 5;
+        }
+        if (y_offset == -40 && !superJump) {
+          up = false;
+          top_of_jump = true;
+        } else if (superJump && y_offset == -80) {
+          up = false;
+          top_of_jump = true;
+        } else if (y_offset == 0) {
+          up = true;
+          is_jumping = false;
+        }
+      }
+
+      // logic for super jumping powers for ifti luigi
+      if (!power_up_picked_up) {
+        draw_transform_power_up(150, 90);
+      }
+      if (x_offset > 120 && x_offset < 160 && y_offset < 30) {
+        if (!power_up_picked_up) {
+          draw_super_jump_power_up_message();
+        }
+        if (t_clicked) {
+          power_up_picked_up = true;
+          superJump = true;
+        }
+      }
+
+      if (is_running_right) {
+          if (is_jumping) {
+            draw_iftimario_jumping_right(x_offset, y_offset);
+          } else {
+            draw_iftimario_running_right(x_offset);
+          }
+        } else if (is_running_left) {
+          if (is_jumping) {
+            draw_iftimario_jumping_left(x_offset, y_offset);
+          } else {
+            draw_iftimario_running_left(x_offset);
+          }
+        } else if (face_left) {
+          if (is_jumping) {
+            draw_iftimario_jumping_left(x_offset, y_offset);
+          } else {
+            draw_iftimario_still_left(x_offset);
+          }
+        } else if (face_right) {
+          if (is_jumping) {
+            draw_iftimario_jumping_right(x_offset, y_offset);
+          } else {
+            draw_iftimario_still_right(x_offset);
+          }
+        }
+
+      // logic for collision with block platform in middle of screen
+      if (x_offset > 100 && x_offset < 192) {
+        if (top_of_jump) {
+          platform = 100;
+          top_of_jump = false;
+        }
+
+      // logic for level 3 stair case
+      } else if (x_offset >= 214 && x_offset < 225) {
+        platform = 151;
+        top_of_jump = false;
+      } else if (x_offset >= 225 && x_offset < 253) {
+        platform = 138;
+        top_of_jump = false;
+      } else if (x_offset >= 253 && x_offset < 281) {
+        platform = 123;
+        top_of_jump = false;
+      } else if (x_offset >= 281 && x_offset < 320) {
+        platform = 110;
+        top_of_jump = false;
+      
+      // default ground level in level 3
+      } else {
+        platform = 167;
+      }
+
+      if (x_offset > 260 && x_offset < 320 && is_jumping && !up && power_up_picked_up) {
+        level_3 = false;
+        level_5_end = true;
+        is_jumping = false;
+        on_lvl5_pipe = true;
+        x_offset = 40;
+        platform = 150;
+      }
+
+    } else if (level_5_end) {
+      draw_level_5_end();
+
+      // ********* logic for PS/2 Keyboard *********
+      // Keyboard input processing. Keyboard input is read one byte at a time.
+      // Every time a read is performed of the PS/2 control register, it
+      // discards the last byte.
+      ps2_data = *(ps2_ctrl_ptr);
+      validRead = ((ps2_data & 0x8000) != 0);
+
+      if (validRead) {
+        // Update the last 3 bytes read to reflect the current read.
+        byte3 = byte2;
+        byte2 = byte1;
+        byte1 = (ps2_data & 0xFF);
+        
+        if (byte1 == 0x29) {
+            spaceBarPressed = true;
+        }
+        if (byte1 == 0x23) {
+          // D = move to the right
+          is_running_right = true;
+          if (x_offset < 295) x_offset += player_speed;
+        } else if (byte1 == 0x1C) {
+          // A = move to the left
+          is_running_left = true;
+          if (x_offset > -15) x_offset -= player_speed;
+        } else if (byte1 == 0x1D) {
+          // W = jump
+          is_jumping = true;
+        } else if (byte1 == 0x1B) {
+          // S = go down pipe
+          s_clicked = true;
+        } else if (byte1 == 0x2C) {
+          // T = pick up power up
+          t_clicked = true;
+        }
+      } else {
+        if (is_running_right) {
+          face_right = true;
+          face_left = false;
+        } else if (is_running_left) {
+          face_left = true;
+          face_right = false;
+        }
+        is_running_right = false;
+        is_running_left = false;
+        s_clicked = false;
+        t_clicked = false;
+      }
+      // ********* end of logic for PS/2 Keyboard *********
+
+      if (is_jumping) {
+        if (up) {
+          if (superJump) {
+            y_offset -= 10;
+          } else {
+            y_offset -= 5;
+          }
+        } else {
+          y_offset += 5;
+        }
+        if (y_offset == -40 && !superJump) {
+          up = false;
+          top_of_jump = true;
+        } else if (superJump && y_offset == -80) {
+          up = false;
+          top_of_jump = true;
+        } else if (y_offset == 0) {
+          up = true;
+          is_jumping = false;
+        }
+      }
+
+      if (is_running_right) {
+          if (is_jumping) {
+            draw_iftimario_jumping_right(x_offset, y_offset);
+          } else {
+            draw_iftimario_running_right(x_offset);
+          }
+        } else if (is_running_left) {
+          if (is_jumping) {
+            draw_iftimario_jumping_left(x_offset, y_offset);
+          } else {
+            draw_iftimario_running_left(x_offset);
+          }
+        } else if (face_left) {
+          if (is_jumping) {
+            draw_iftimario_jumping_left(x_offset, y_offset);
+          } else {
+            draw_iftimario_still_left(x_offset);
+          }
+        } else if (face_right) {
+          if (is_jumping) {
+            draw_iftimario_jumping_right(x_offset, y_offset);
+          } else {
+            draw_iftimario_still_right(x_offset);
+          }
+        }
+
+      // logic for level 5 end stair case
+      if (x_offset > (94 - 20) && x_offset < (101 - 20)) {
+        platform = (202 - 40);
+        top_of_jump = false;
+      } else if (x_offset >= (101 - 20) && x_offset < (108 - 20)) {
+        platform = (194 - 40);
+        top_of_jump = false;
+      } else if (x_offset >= (108 - 20) && x_offset < (115 - 20)) {
+        platform = (188 - 40);
+        top_of_jump = false;
+      } else if (x_offset >= (115 - 20) && x_offset < (122 - 20)) {
+        platform = (180 - 40);
+        top_of_jump = false;
+      } else if (x_offset >= (122 - 20) && x_offset < (129 - 20)) {
+        platform = (172 - 40);
+        top_of_jump = false;
+      } else if (x_offset >= (129 - 20) && x_offset < (136 - 20)) {
+        platform = (166 - 40);
+        top_of_jump = false;
+      } else if (x_offset >= (136 - 20) && x_offset < (152 - 20)) {
+        platform = (158 - 40);
+        top_of_jump = false;
+
+      } else if (x_offset >= (152) && x_offset <= (162) && is_jumping && top_of_jump) {
+          top_of_jump = false;
+          flag_takedown = true;
+      
+      //default found level in level-5 end
+      } else if (x_offset >= 248 && x_offset <= 260 && spaceBarPressed) {
+        isGameOverWait = true;
+        level_5_end = false;
+      }
+      
+      else {
+        platform = 169;
+      }
+
+      if (flag_takedown) {
+        flag_y = flag_y + 5;
+        draw_flag(180,flag_y);
+        if (flag_y == 140) {
+            audio_playback_mono(game_cleared, 46184);
+            flag_takedown = false;
+        }
+      } else {
+        draw_flag(180,flag_y);
+      }
+
+    } else if (isGameOverWait) {
+        draw_level_5_end();
+
+        // ********* logic for PS/2 Keyboard *********
+        // Keyboard input processing. Keyboard input is read one byte at a time.
+        // Every time a read is performed of the PS/2 control register, it
+        // discards the last byte.
+        ps2_data = *(ps2_ctrl_ptr);
+        validRead = ((ps2_data & 0x8000) != 0);
+
+        if (validRead) {
+            // Update the last 3 bytes read to reflect the current read.
+            byte3 = byte2;
+            byte2 = byte1;
+            byte1 = (ps2_data & 0xFF);
+            
+            if (byte1 == 0x5A) {
+                enterBarPressed = true;
+            }
+        } else {
+            enterBarPressed = false;
+        }
+
+        draw_flag(180,flag_y);
+        draw_flag_bd(280, 100);
+        if (flagWaveCount >= 10) {
+          draw_game_over_success();
+          isGameOverScreen = true;
+        }
+        flagWave = !flagWave;
+        flagWaveCount++;
+
+        if (enterBarPressed && isGameOverScreen) {
+            isGameOverWait = false;
+            level_1 = true;
+            enterBarPressed  = false;
+            isStartScreen = true;
+            playStartSound = true;
+        }
+
     }
 
     // waiting stage for buffer swapping
     wait_for_vsync();  // swap front and back buffers on VGA vertical sync
     pixel_buffer_start = *(pixel_ctrl_ptr + 1);  // new back buffer
-  }
+  } 
 }
 
 void swap(int *a, int *b) {
   int temp = *(a);
   *a = *b;
   *b = temp;
+}
+
+void draw_start_screen() {
+    for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 320; x++) {
+      int index = y * 320 + x;
+      if (Super_Ifti_Bros_start_screen[index] == 0x0000) {
+        plot_pixel(x, y, Level1[index]);
+      } else {
+        plot_pixel(x, y, Super_Ifti_Bros_start_screen[index]);
+      }
+      
+    }
+  }
 }
 
 void draw_level_1() {
@@ -370,6 +797,64 @@ void draw_level_2() {
     for (int x = 0; x < 320; x++) {
       int index = y * 320 + x;
       plot_pixel(x, y, Level2[index]);
+    }
+  }
+}
+
+void draw_level_3() {
+  for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 320; x++) {
+      int index = y * 320 + x;
+      plot_pixel(x, y, Level_3[index]);
+    }
+  }
+}
+
+void draw_game_over_success() {
+  for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 320; x++) {
+      int index = y * 320 + x;
+      if (game_over_success[index] != 0x0000) {
+       plot_pixel(x, y, game_over_success[index]);
+      }
+    }
+  }
+}
+
+void draw_level_5_end() {
+ for (int y = 0; y < 240; y++) {
+    for (int x = 0; x < 320; x++) {
+      int index = y * 320 + x;
+      plot_pixel(x, y, Level_5_end[index]);
+    }
+  }
+}
+
+void draw_flag_bd(x_offset, y_offset) {
+ for (int y = 0; y < 40; y++) {
+    for (int x = 0; x < 44; x++) {
+      int index = y * 44 + x;
+      if (!flagWave) {
+        if (flag_bd[index] != 0xFFFF) {
+            plot_pixel(x + x_offset, y + y_offset, flag_bd[index]);
+        }
+      } else {
+        if (flag_bd_flipped[index] != 0xFFFF) {
+            plot_pixel(x + (x_offset - 26), y + y_offset, flag_bd_flipped[index]);
+        }
+      }
+    }
+  }
+}
+
+void draw_flag(x_offset, y_offset) {
+ for (int y = 0; y < 40; y++) {
+    for (int x = 0; x < 44; x++) {
+      int index = y * 44 + x;
+      if (flag[index] != 0xFFFF) {
+        plot_pixel(x + x_offset, y + y_offset, flag[index]);
+      }
+      
     }
   }
 }
@@ -510,12 +995,23 @@ void draw_iftimario_jumping_left(x_offset, y_offset) {
   }
 }
 
-void draw_transform_power_up() {
+void draw_transform_power_up(int x_offset, int y_offset) {
   for (int y = 0; y < 40; y++) {
     for (int x = 0; x < 40; x++) {
       int index = y * 40 + x;
       if (transfrom_power_up[index] != 0xFFFF) {
-        plot_pixel(x + 140, y + 150, transfrom_power_up[index]);
+        plot_pixel(x + x_offset, y + y_offset, transfrom_power_up[index]);
+      }
+    }
+  }
+}
+
+void draw_super_jump_power_up_message() {
+  for (int y = 0; y < 40; y++) {
+    for (int x = 0; x < 70; x++) {
+      int index = y * 70 + x;
+      if (super_jump_message[index] != 0x0000) {
+        plot_pixel(x + 150, y + 90, super_jump_message[index]);
       }
     }
   }
@@ -530,6 +1026,17 @@ void draw_transform_power_up_message() {
       }
     }
   }
+}
+
+void audio_playback_mono(int *samples, int n) {
+    audiop->control = 0x8; // clear the output FIFOs
+    audiop->control = 0x0; // resume input conversion
+    for (int i = 0; i < n; i++) {
+        // wait till there is space in the output FIFO
+        while (audiop->warc == 0);
+        audiop->ldata = samples[i];
+        audiop->rdata = samples[i];
+    }
 }
 
 void draw_box(int x, int y, int short line_color) {
